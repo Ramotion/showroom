a10a857051ecd72c1095ac50744616a2eafc1261
Alex.k <Alex.k@ramotion.com>
2016-12-12 12:33:11 +0300

    fixed bug with about view

--- Showroom/ViewControllers/AboutView/AboutView.swift
+++ Showroom/ViewControllers/AboutView/AboutView.swift
@@ -47,12 +47,16 @@ extension AboutView {
   
   func show(on view: UIView, completion: @escaping () -> Void) {
     
+    
     if circleView == nil { circleView = .build(on: self, position: titleView.infoButton.center) }
     scrollView.alpha = 1
     scrollView.contentOffset.y = 0
     
     view.addSubview(self)
     self <- Edges(0)
+    
+    view.layoutIfNeeded()
+    print(infoText.bounds.size.height)
     
     view.bringSubview(toFront: titleView)
     
@@ -86,19 +90,21 @@ extension AboutView {
     infoText.pop_removeAllAnimations()
     infoText.alpha = 0
     infoText.animate(duration: 0.5, delay: 0.3, [.alphaFrom(0, to: 1, removed: false)])
-    let from = titleLabelTopConstraint.constant + titleLabelHeight.constant + infoTextHeightconstraint.constant / 2 + infoTextTopConstraint.constant + sharedViewHeightConstraint.constant / 2
+    let from = titleLabelTopConstraint.constant + titleLabelHeight.constant + infoText.bounds.size.height / 2 + infoTextTopConstraint.constant + sharedViewHeightConstraint.constant / 2
     infoText.animate(duration: 0.7, delay: 0.3, [.layerPositionY(from: from, to: from - sharedViewHeightConstraint.constant / 2)])
     
     learnMoreLabel.pop_removeAllAnimations()
     learnMoreLabel.alpha = 0
     learnMoreLabel.animate(duration: 0.5, delay: 0.3, [.alphaFrom(0, to: 1, removed: false)])
-    let infofrom = titleLabelTopConstraint.constant + titleLabelHeight.constant + infoTextHeightconstraint.constant + infoTextTopConstraint.constant 
+    let infofrom = titleLabelTopConstraint.constant + titleLabelHeight.constant + infoText.bounds.size.height + infoTextTopConstraint.constant 
     let lfrom = infofrom + learnMoreSpace.constant + learnMoreHeight.constant / 2 + sharedViewHeightConstraint.constant / 2
     learnMoreLabel.animate(duration: 0.7, delay: 0.3, [.layerPositionY(from: lfrom, to: lfrom - sharedViewHeightConstraint.constant / 2)])
     
   }
   
   func hide(on view: UIView, completion: @escaping () -> Void) {
+    
+    print(infoText.bounds.size.height)
    
     scrollView.animate(duration: 0.3, [.alpha(to: 0)], timing: .easyInEasyOut)
     transperentView.animate(duration: 0.4, [.alphaFrom(0.4, to: 0, removed: false)])
@@ -138,9 +144,10 @@ private extension AboutView {
     guard let font = UIFont(name: "Graphik-Regular", size: 16) else { return }
     let style = NSMutableParagraphStyle()
     style.lineSpacing = 7
-    infoText.attributedText = text.withAttributes([
+    let attributedText = text.withAttributes([
       .paragraphStyle(style),
       .font(font)]
     )
+    infoText.attributedText = attributedText
   }
 }
