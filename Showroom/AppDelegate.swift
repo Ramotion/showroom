17ab25c49f5c6099539135738cd1c382a2255c65
Abdurahim Jauzee <jauzee@ya.ru>
2017-04-14 10:23:25 +0300

    customize ElongationPreview configuration

--- Showroom/AppDelegate.swift
+++ Showroom/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import ElongationPreview
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -63,4 +64,27 @@ extension AppDelegate {
       ]
     }
   }
+  
+  fileprivate func configureElongationPreviewControl() {
+    // Customize ElongationConfig
+    var config = ElongationConfig()
+    config.scaleViewScaleFactor = 0.9
+    config.topViewHeight = 190
+    config.bottomViewHeight = 170
+    config.bottomViewOffset = 20
+    config.parallaxFactor = 100
+    config.separatorHeight = 0.5
+    config.separatorColor = UIColor.white
+    
+    // Durations for presenting/dismissing detail screen
+    config.detailPresetingDuration = 0.4
+    config.detailDismissingDuration = 0.4
+    
+    // Customize behaviour
+    config.headerTouchAction = .collpaseOnBoth
+    
+    // Save created appearance object as default
+    ElongationConfig.shared = config
+  }
+  
 }
