7ecf6613ed072371da885667ffaf625911c3fdc6
Dmitry Nesterenko <chebur@Macintosh.local>
2018-07-02 16:57:46 +0300

    Mock dribbble user and shots responses

--- Showroom/Utilities/Networking/NetworkingManager.swift
+++ Showroom/Utilities/Networking/NetworkingManager.swift
@@ -9,7 +9,6 @@ final class NetworkingManager {
         static let base = "https://api.dribbble.com/v2/"
         static let user = base + "user"
         static let shots = base + "user/shots"
-        static let popularShots = base + "popular_shots"
     }
 }
 
@@ -17,18 +16,88 @@ final class NetworkingManager {
 extension NetworkingManager {
     
     func fetchDribbbleUser() -> Observable<User> {
-        return fetch(getUrl: URLString.user)
-    }
-
-    func fetchDribbblePopularShots() -> Observable<[Shot]> {
-        let request = URLRequest(url: URL(string: URLString.popularShots)!)
-        return URLSession.shared.rx
-            .data(request: request)
-            .map { try JSONDecoder().decode([Shot].self, from: $0) }
+        let data = """
+{
+    "id": 0,
+    "name": "name",
+    "html_url": "https://cdn.dribbble.com/users/824641/screenshots/4773637",
+}
+""".data(using: .utf8)!
+        
+        return Observable<User>.create { subscribtion in
+            do {
+                let object = try JSONDecoder().decode(User.self, from: data)
+                subscribtion.onNext(object)
+                subscribtion.onCompleted()
+            } catch {
+                subscribtion.onError(error)
+            }
+            
+            return Disposables.create()
+        }
+        // TODO: temporarily replace fetch call with mock object
+//        return fetch(getUrl: URLString.user)
     }
     
     func fetchDribbbleShots() -> Observable<[Shot]> {
-        return fetch(getUrl: URLString.shots, parametrs: ["per_page=200"])
+        let data = """
+[
+  {
+    "id": 0,
+    "description": "description",
+    "html_url": "https://cdn.dribbble.com/users/824641/screenshots/4773637",
+    "title": "title",
+    "images": {
+      "normal": "https://cdn.dribbble.com/users/824641/screenshots/4773637/3dflip_teaser.gif"
+    },
+    "animated": true
+  },
+  {
+    "id": 0,
+    "description": "description",
+    "html_url": "https://cdn.dribbble.com/users/824641/screenshots/4773637",
+    "title": "title",
+    "images": {
+      "normal": "https://cdn.dribbble.com/users/824641/screenshots/4773637/3dflip_teaser.gif"
+    },
+    "animated": true
+  },
+  {
+    "id": 0,
+    "description": "description",
+    "html_url": "https://cdn.dribbble.com/users/824641/screenshots/4773637",
+    "title": "title",
+    "images": {
+      "normal": "https://cdn.dribbble.com/users/824641/screenshots/4773637/3dflip_teaser.gif"
+    },
+    "animated": true
+  },
+  {
+    "id": 0,
+    "description": "description",
+    "html_url": "https://cdn.dribbble.com/users/824641/screenshots/4773637",
+    "title": "title",
+    "images": {
+      "normal": "https://cdn.dribbble.com/users/824641/screenshots/4773637/3dflip_teaser.gif"
+    },
+    "animated": true
+  }
+]
+""".data(using: .utf8)!
+        
+        return Observable<[Shot]>.create { subscribtion in
+            do {
+                let object = try JSONDecoder().decode([Shot].self, from: data)
+                subscribtion.onNext(object)
+                subscribtion.onCompleted()
+            } catch {
+                subscribtion.onError(error)
+            }
+            
+            return Disposables.create()
+        }
+        // TODO: temporarily replace fetch call with mock object
+//        return fetch(getUrl: URLString.shots, parametrs: ["per_page=200"])
     }
     
     private func fetch<T>(getUrl: String, parametrs: [String]? = nil) -> Observable<T> where T: Codable {
