011391f94379c894b838a09f7c71dd3afc1e0504
Dmitry Nesterenko <chebur@Macintosh.local>
2018-07-27 16:38:43 +0300

    Add progress handler

--- Showroom/Utilities/Images/UIImageView+Extension.swift
+++ Showroom/Utilities/Images/UIImageView+Extension.swift
@@ -38,32 +38,36 @@ extension UIImageView {
         }
     }
     
-    func setImage(url: URL, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
+    func setImage(url: URL, progress: ProgressHandler? = nil, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
         var request = Nuke.Request(url: url)
+        request.progress = progress
         if let processor = processor {
             request.process(key: processor.0, processor.1)
         }
         setImage(request: request, handler: handler)
     }
     
-    func setImage(urlRequest: URLRequest, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
+    func setImage(urlRequest: URLRequest, progress: ProgressHandler? = nil, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
         var request = Nuke.Request(urlRequest: urlRequest)
+        request.progress = progress
         if let processor = processor {
             request.process(key: processor.0, processor.1)
         }
         setImage(request: request, handler: handler)
     }
     
-    func setImage(url: URL, targetSize: CGSize, contentMode: Decompressor.ContentMode, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
+    func setImage(url: URL, targetSize: CGSize, contentMode: Decompressor.ContentMode, progress: ProgressHandler? = nil, processor: (String, ((UIImage) -> UIImage?))? = nil, handler: Manager.Handler? = nil) {
         var request = Nuke.Request(url: url, targetSize: targetSize, contentMode: contentMode)
+        request.progress = progress
         if let processor = processor {
             request.process(key: processor.0, processor.1)
         }
         setImage(request: request, handler: handler)
     }
     
-    func setImage(urlRequest: URLRequest, targetSize: CGSize, contentMode: Decompressor.ContentMode, processor: (String, ((UIImage) -> UIImage?))? = nil , handler: Manager.Handler? = nil) {
+    func setImage(urlRequest: URLRequest, targetSize: CGSize, contentMode: Decompressor.ContentMode, progress: ProgressHandler? = nil, processor: (String, ((UIImage) -> UIImage?))? = nil , handler: Manager.Handler? = nil) {
         var request = Nuke.Request(urlRequest: urlRequest, targetSize: targetSize, contentMode: contentMode)
+        request.progress = progress
         if let processor = processor {
             request.process(key: processor.0, processor.1)
         }
