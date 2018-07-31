11902ca4eaf7481be796273e0f73b410f6dbbda8
Dmitry Nesterenko <chebur@Macintosh.local>
2018-07-31 19:36:41 +0300

    Implement loading animation

--- Showroom/DribbbleShots/Cells/DribbbleShotCell.swift
+++ Showroom/DribbbleShots/Cells/DribbbleShotCell.swift
@@ -7,6 +7,7 @@ final class DribbbleShotCell: UICollectionViewCell {
     @IBOutlet weak private var shadowImageView: UIImageView!
     @IBOutlet weak private var containerView: UIView!
     @IBOutlet weak private var imageView: DribbbleShotImageView!
+    @IBOutlet weak private var loadingView: UIView!
     @IBOutlet weak private var gifImageView: UIImageView!
 
     var state: DribbbleShotState = .wireframe {
@@ -14,20 +15,32 @@ final class DribbbleShotCell: UICollectionViewCell {
             switch state {
             case .default(let shot):
                 setNeedsLayout()
-                containerView.backgroundColor = nil
-                imageView.isTransparentOverlayVisible = false
+                loadingView.alpha = 0
                 updateWithShot(shot)
             case .sent(let shot):
                 setNeedsLayout()
-                containerView.backgroundColor = nil
-                imageView.isTransparentOverlayVisible = true
+                loadingView.alpha = 0
                 updateWithShot(shot)
             case .wireframe:
                 imageView.cancelImageRequest()
-                containerView.backgroundColor = UIColor(red: 210 / 255.0, green: 94 / 255.0, blue: 141 / 255.0, alpha: 1)
-                imageView.isTransparentOverlayVisible = false
+                loadingView.alpha = 0
                 updateWithShot(nil)
             }
+            updateTransparentOverlayVisibility()
+        }
+    }
+    
+    private func updateTransparentOverlayVisibility() {
+        guard !isLoading else {
+            imageView.isTransparentOverlayVisible = true
+            return
+        }
+        
+        switch state {
+        case .default, .wireframe:
+            imageView.isTransparentOverlayVisible = false
+        case .sent:
+            imageView.isTransparentOverlayVisible = true
         }
     }
     
@@ -40,6 +53,7 @@ final class DribbbleShotCell: UICollectionViewCell {
         super.awakeFromNib()
         
         backgroundColor = nil
+        containerView.backgroundColor = UIColor(white: 1, alpha: 0.1)
     }
     
     override func prepareForReuse() {
@@ -58,7 +72,11 @@ final class DribbbleShotCell: UICollectionViewCell {
 
         // image url
         if let imageUrl = state.imageUrl {
-            imageView.setImage(url: imageUrl, targetSize: imageView.bounds.size, contentMode: .aspectFill)
+            startImageLoadingAnimation()
+            imageView.setImage(url: imageUrl, targetSize: imageView.bounds.size, contentMode: .aspectFill, handler: { [weak self] result, fromCache in
+                self?.imageView.image = result.value
+                self?.stopImageLoadingAnimation(completion: nil)
+            })
         } else {
             imageView.cancelImageRequest()
         }
@@ -85,11 +103,58 @@ final class DribbbleShotCell: UICollectionViewCell {
         // shadow
         do {
             let image = UIImage.image(size: shadowImageView.bounds.size) { context in
+                UIColor(red: 254 / 255.0, green: 55 / 255.0, blue: 138 / 255.0, alpha: 1).setFill()
                 context.cgContext.setShadow(offset: CGSize(width: 0, height: 3), blur: 25, color: UIColor(white: 0, alpha: 0.12).cgColor)
                 UIBezierPath(roundedRect: containerView.frame, cornerRadius: kContentViewCornerRadius).fill()
             }
             shadowImageView.image = image
         }
+    }
+    
+    private var isLoading = false
+    
+    private func startImageLoadingAnimation() {
+        guard !isLoading else { return }
+        isLoading = true
+        
+        animateLoadingView()
+    }
+    
+    private func animateLoadingView() {
+        updateTransparentOverlayVisibility()
+        imageView.alpha = 0
+        loadingView.alpha = 1
+
+        let initialFrame = containerView.bounds.offsetBy(dx: -containerView.bounds.width, dy: 0)
+        loadingView.frame = initialFrame
+        
+        UIView.animate(withDuration: 0.6, delay: 0, options: [.curveEaseInOut], animations: {
+            self.loadingView.frame = initialFrame.offsetBy(dx: initialFrame.width, dy: 0)
+        }, completion: { [weak self] finished in
+            let shouldCompleteAnimation = self?.shouldCompleteAnimation ?? false
+            
+            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
+                self?.loadingView.frame = initialFrame.offsetBy(dx: initialFrame.width * 2, dy: 0)
+                self?.imageView.alpha = 1
+                self?.loadingView.alpha = 0
+                self?.updateTransparentOverlayVisibility()
+            }, completion: { finished in
+                if shouldCompleteAnimation {
+                    self?.loadingAnimationCompletion?()
+                    self?.loadingAnimationCompletion = nil
+                } else {
+                    self?.animateLoadingView()
+                }
+            })
+        })
+    }
+    
+    private var loadingAnimationCompletion: (() -> ())?
+    private var shouldCompleteAnimation = false
+    
+    private func stopImageLoadingAnimation(completion: (() -> ())?) {
+        loadingAnimationCompletion = completion
+        shouldCompleteAnimation = true
     }
     
 }
